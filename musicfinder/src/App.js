58f1aac65cac7d630aa0f08e5b025e2a34831c96
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-04-25 19:42:00 -0400

    Minor bug fixed

--- musicfinder/src/App.js
+++ musicfinder/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './App.css';
 import { Route,} from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
 import Register from './components/register/Register.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1> Welcome to Music Finder</h1>
-        <Route path ='/api/register' component = {Register} />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1> Welcome to Music Finder</h1>
+            <Route path ='/api/register' component = {Register} />
+        </header>
+      </div>
+    </Router>
   );
 }
 
