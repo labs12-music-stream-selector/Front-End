289682bf783915c52a216786ef70ee5e4639329a
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-04-25 19:46:17 -0400

    Minor update for styled component

--- musicfinder/src/components/register/Register.js
+++ musicfinder/src/components/register/Register.js
@@ -1,38 +1,36 @@
 import React from 'react';
 import axios from 'axios';
-const baseUrl = process.env.REACT_APP_FE_URL || "https://fantabulous-music-finder.herokuapp.com";
+import styled from 'styled-components';
+const baseUrl = process.env.REACT_APP_FE_URL || "https://fantabulous-music-finder.herokuapp.com/";
 class Register extends React.Component {
   constructor() {
-      super();
-      this.state = {
-          username: '',
-          password: '',
-          confirmPassword: '',
-          email: '',
-      };
+    super();
+    this.state = {
+        username: '',
+        email: '',
+        password: '',
+        
+    };
   }
   handleInput = event => {
     this.setState({ [event.target.name]: event.target.value })
   };
   handleSubmit = e => {
-    if (this.state.confirmPassword === this.state.password){
-       axios
+      axios
         .post(`${baseUrl}/api/register`, this.state)
         .then(res => { 
           console.log(res);
-          this.props.history.push(`api/login`);
-       })
-       .catch(err => console.log(err),
-       )
-    } else {
-        console.log("Password doesn't match")
-    }
- }
+          this.props.history.push(`/api/login`);
+      })
+      .catch(err => console.log(err),
+      )
+  }
+
   render(){
     return(
-      <div className ='login-box'>
-          <form  className ='login-form' onSubmit={this.handleSubmit}>
-          <div className='input-form' >
+      <RegBar className ='login-box'>
+          <form  className ='login-form' onSubmit={e => e.preventDefault()}>
+          <div className='input-box' >
               <input 
                   className ='input-form'
                   type="text"
@@ -45,10 +43,11 @@ class Register extends React.Component {
                   className ='input-form'
                   type="text"
                   placeholder="email"
-                  name="username"
+                  name="email"
                   value={this.state.email}
                   onChange={this.handleInput}
               />
+              
               <input
                   className ='input-form'
                   type= 'password'
@@ -56,22 +55,24 @@ class Register extends React.Component {
                   name='password'
                   value={this.state.password}
                   onChange={this.handleInput} 
-              />
-            <input
-                  className ='input-form'
-                  type= 'password'
-                  placeholder= 'Confirm Password'
-                  name='confirmPassword'
-                  value={this.state.confirmPassword}
-                  onChange={this.handleInput} 
-              /> 
-              
-              <button  onClick={this.handleSubmit}> Register</button>  
+              />  
+              <button onClick={this.handleSubmit}> Register</button>  
               </div>  
           </form>
-      </div>
+      </RegBar>
     );
   }
 }  
 export default Register;
 
+
+const RegBar = styled.div`
+    .input-form{
+        margin: 5px;
+    }
+    button{
+      background: #EB5757;
+      border-radius: 5px;
+      color : white;
+    }
+`
