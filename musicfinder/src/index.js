4cc4e273616f2915d5f31fb1cac6c6b620dc712a
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-26 14:05:45 -0400

    Removed dotenv
    turns out its not needed...

--- musicfinder/src/index.js
+++ musicfinder/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter as Router, withRouter} from 'react-router-dom';
 import App from './App';
-require('dotenv').config();
 
 const AppWithRouter = withRouter(App);
 
