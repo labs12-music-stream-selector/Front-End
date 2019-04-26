134f9827749856f9e466f63d2988d95426be7703
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-26 17:37:51 -0400

    Added placeholder for DS data

--- musicfinder/src/App.js
+++ musicfinder/src/App.js
@@ -4,12 +4,14 @@ import { Route} from 'react-router-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
 import Register from './components/register/Register.js';
 import Login from './components/LoginForm/LoginForm.js';
+import DSdata from './components/DSdata/DSdata.js';
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="App-header">
           <h1> Welcome to Music Finder</h1>
+          <DSdata/>
           <Route path ='/register' component = {Register} />
           <Route path ='/login' component = {Login} />
         </header>
