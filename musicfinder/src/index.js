e93d86048c2d8f66103351f50b47c37f07c547e1
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-26 12:52:16 -0400

    added dotenv

--- musicfinder/src/index.js
+++ musicfinder/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import {BrowserRouter as Router, withRouter} from 'react-router-dom';
 import App from './App';
+require('dotenv').config();
 
 const AppWithRouter = withRouter(App);
 
