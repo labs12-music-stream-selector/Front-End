03c9c41944ff0bb315b6359481babcb5f042c067
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-30 18:10:41 -0400

    added functions for search

--- musicfinder/src/components/Browser/Browser.js
+++ musicfinder/src/components/Browser/Browser.js
@@ -1,12 +1,13 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import YouTubePlayer from '../YoutubePlayer/YoutubePlayer.js';
+import SearchBar from '../SearchBar/SearchBar.js';
 
 const Browser = (props) => {
 	const [tracks, updateTracks] = useState([{id:'ErISjXq9hYg'},{id:'ErISjXq9hYg'},{id:'ErISjXq9hYg'}]);
 
 	useEffect(() => {
-		// TODO: replalce with correct url
+		// TODO: replalce with correct url to get initial tracks
 		// const url = 'url to get tracks';
 		// updateTracks(getTracks(url));
 	},[])
@@ -17,6 +18,7 @@ const Browser = (props) => {
 			flexWrap: 'wrap',
 			justifyContent: 'spaceBetween',
 		}}>
+			<SearchBar searchTrack = {searchTrack}/>
 			{tracks.map(track => {
 				return <YouTubePlayer url = {track.id}/>
 			})}
@@ -27,6 +29,14 @@ const Browser = (props) => {
 	async function getTracks(url){
 		const res = await axios.get(url);
 		return res.data;
+	}
+
+	async function searchTrack(searchTerm){
+		//TODO: replace correct url
+		// const url = 'DS url'
+		// const res = await axios.get(url);
+		// updateTracks(res.data)
+		console.log(searchTerm)
 	}
 } 
 
