66f3a0959f20ad239099b3c45e7b96fece2a21b2
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-04-30 15:22:33 -0700

    minor bug fix

--- musicfinder/src/components/YoutubePlayer/YoutubePlayer.js
+++ musicfinder/src/components/YoutubePlayer/YoutubePlayer.js
@@ -7,20 +7,16 @@ const YoutubePlayer = (props) => {
   let playlist = props.playlist;
 
   function urlConstructor(id, playlist) {
-    if (playlist) { 
+    if (playlist) {
       return `${id}?list=${playlist}`;
-    } else {       
+    } else {
       return `${id}`
-    }   
+    }
   }
 
   return (
     <>
-<<<<<<< HEAD
-      <iframe width="400" height="300" src={`https://www.youtube.com/embed/${urlConstructor(url, playlist)}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
-=======
       <iframe width="400" height="300" src={`https://www.youtube.com/embed/${urlConstructor(props.url, props.playlist)}`} allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
->>>>>>> c5dfacecda5d522e8428c0d36600cc09d726a963
     </>
   )
 }
