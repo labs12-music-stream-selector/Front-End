c3ade26679ed6e227f519c4a454c7eb9600f3889
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-04-30 16:26:33 -0400

    created Browser component

---
+++ musicfinder/src/components/Browser/Browser.js
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import YouTubePlayer from '../YoutubePlayer/YoutubePlayer.js';
+
+const Browser = (props) => {
+	const [tracks, updateTracks] = useState([{id:'ErISjXq9hYg'},{id:'ErISjXq9hYg'},{id:'ErISjXq9hYg'}]);
+
+	useEffect(() => {
+		// const url = 'url to get tracks';
+		// updateTracks(getTracks(url));
+	},[])
+	
+	return (
+		<div style = {{
+			display: 'flex',
+			flexWrap: 'wrap',
+			justifyContent: 'spaceBetween',
+		}}>
+			{tracks.map(track => {
+				return <YouTubePlayer url = {track.id}/>
+			})}
+
+		</div>
+	);
+
+	async function getTracks(url){
+		const res = await axios.get(url);
+		return res.data;
+	}
+} 
+
+export default Browser;
