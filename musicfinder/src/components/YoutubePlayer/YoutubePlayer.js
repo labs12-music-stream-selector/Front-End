5f003bd04e6208e86452fe9c84a0e31bf0d3f54a
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-04-30 14:58:48 -0700

    fixed a warning on youtube player

--- musicfinder/src/components/YoutubePlayer/YoutubePlayer.js
+++ musicfinder/src/components/YoutubePlayer/YoutubePlayer.js
@@ -6,16 +6,16 @@ const YoutubePlayer = (props) => {
   let playlist = props.playlist;
 
   function urlConstructor(id, playlist) {
-    if (playlist) {
+    if (playlist) { 
       return `${id}?list=${playlist}`;
-    } else {
+    } else {       
       return `${id}`
-    }
+    }   
   }
 
   return (
     <>
-      <iframe width="400" height="300" src={`https://www.youtube.com/embed/${urlConstructor(url, playlist)}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="400" height="300" src={`https://www.youtube.com/embed/${urlConstructor(url, playlist)}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
     </>
   )
 }
