54ccc5a71738db17c4d0875c3da3be312996ee12
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-05-01 15:33:07 -0400

    added reuseable select component

---
+++ musicfinder/src/components/Select/Select.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Select = (props) => {
+
+	return (
+		<select onChange = {handleChange}>
+			<option value = 'select'>Select Mood</option>
+			{props.options.map(option => {
+				return <option value = {option}>{option}</option>
+			})}
+		</select>
+	);
+
+	function handleChange(e){
+		if(e.target.value !== 'select'){
+			props.getTracks(e.target.value);
+		}
+	}
+}
+
+export default Select;
