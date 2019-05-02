5dd5e507c34533702339d113e813d9023e253be7
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-05-02 17:58:02 -0400

    added LandingPage component

---
+++ musicfinder/src/components/LandingPage/LandingPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const LandingPage = (props) => {
+	return (
+		<>
+			<h1>MoodiBeats</h1>
+			<div>
+				<h2>Find copyright free music by mood.</h2>
+			</div>
+		</>
+	)
+}
+
+export default LandingPage;
