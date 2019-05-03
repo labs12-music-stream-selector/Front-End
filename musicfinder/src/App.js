9e19753a96a82b84ae0c7fd324725e9b4c7be1d9
Md Kawsar Hussen <mdkawsarhussen@Mds-MacBook-Pro.local>
2019-05-02 20:02:21 -0400

    Resolve merge conflict

--- musicfinder/src/App.js
+++ musicfinder/src/App.js
@@ -9,17 +9,15 @@ import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy.js";
 import TermsOfService from "./components/TermsOfService/TermsOfService.js";
 import SignOut from './components/SignOut/SignOut.js'
 import Browser from './components/Browser/Browser.js';
-<<<<<<< HEAD
 import User from './components/user/User.js'
-=======
 import LandingPage from './components/LandingPage/LandingPage.js';
->>>>>>> 461b3ef369d7769b4c91be29154e968e886105ce
 
 function App() {
   return (
     <Router>
       <div className="App">
         <header className="">
+
           <Route path="/register" component={Register} />
           <Route path="/login" component={Login} />
           <Route path="/player" component={YoutubePlayer} />
@@ -29,9 +27,10 @@ function App() {
           <Route path="/user" component={User} />
           <Route path="/browser" component={Browser} />
         </header>
+
         <Route exact path='/' component={LandingPage}/>
+
       </div>
-      
     </Router>
   );
 }
