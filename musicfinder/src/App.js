fb52168566d18e4d41f59b4ea3c8f6d0e23eb368
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-05-07 11:53:17 -0600

    Getting user Id from Youtube.

--- musicfinder/src/App.js
+++ musicfinder/src/App.js
@@ -6,28 +6,30 @@ import Login from "./components/LoginForm/OAuthGoogle.js";
 import YoutubePlayer from "./components/YoutubePlayer/YoutubePlayer.js";
 import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy.js";
 import TermsOfService from "./components/TermsOfService/TermsOfService.js";
-import SignOut from './components/SignOut/SignOut.js'
-import Browser from './components/Browser/Browser.js';
-import User from './components/user/User.js'
-import LandingPage from './components/LandingPage/LandingPage.js';
-import styled from 'styled-components';
+import SignOut from "./components/SignOut/SignOut.js";
+import Browser from "./components/Browser/Browser.js";
+import User from "./components/user/User.js";
+import LandingPage from "./components/LandingPage/LandingPage.js";
+import styled from "styled-components";
 
-import Patreon from './components/Patreon/Patreon';
+import Patreon from "./components/Patreon/Patreon";
+
+import NewPlaylist from "./components/youTubeUser/NewPlaylist.js";
 
 function App() {
   return (
     <Router>
       <RouterDiv className="App">
         <header className="">
-          <nav className="navBar" style={{ width: '100%', display: 'flex' }}>
-            <Link to='/'>Home</Link>
-            <Link to='/login'>Login</Link>
-            <Link to='/browser'>Browse Songs</Link>
+          <nav className="navBar" style={{ width: "100%", display: "flex" }}>
+            <Link to="/">Home</Link>
+            <Link to="/login">Login</Link>
+            <Link to="/browser">Browse Songs</Link>
             {/* <Link to='/privacypolicy'>Legal Stuff</Link> */}
-            <Link to='/termsofservice'>Terms & Services</Link>
-            <Link to='/user'>Your Profile</Link>
-            <Link to='/signout'>Log Out</Link>
-            <Link to='/support-us'>Support Us</Link>
+            <Link to="/termsofservice">Terms & Services</Link>
+            <Link to="/user">Your Profile</Link>
+            <Link to="/signout">Log Out</Link>
+            <Link to="/support-us">Support Us</Link>
           </nav>
           <Route path="/login" component={Login} />
           <Route path="/player" component={YoutubePlayer} />
@@ -38,35 +40,31 @@ function App() {
           <Route path="/browser" component={Browser} />
           <Route path="/support-us" component={Patreon} />
         </header>
-
-        <Route exact path='/' component={LandingPage} />
-
+        <NewPlaylist />;
+        <Route exact path="/" component={LandingPage} />
       </RouterDiv>
     </Router>
   );
 }
 export default App;
 
-
 const RouterDiv = styled.div`
-    .navBar{
-        display: flex;
-        flex-wrap: warp;
-        background : black;
-        width : 100%;
-        justify-content : space-between;
-        a{
-          text-decoration: none;
-          margin: 20px;
-          min-height : 25px;
-          color : white;
-        }
-        a:hover{
-          color : white;
-          border-radius : 5px;
-          border: 1px solid grey;
-        }
+  .navBar {
+    display: flex;
+    flex-wrap: warp;
+    background: black;
+    width: 100%;
+    justify-content: space-between;
+    a {
+      text-decoration: none;
+      margin: 20px;
+      min-height: 25px;
+      color: white;
     }
-
-
-`
+    a:hover {
+      color: white;
+      border-radius: 5px;
+      border: 1px solid grey;
+    }
+  }
+`;
