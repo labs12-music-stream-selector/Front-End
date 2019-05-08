c67414a6ae342fe763a7118a4542e35cc647fe9e
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-05-08 10:34:38 -0600

    copy/paste app.js from master

--- musicfinder/src/App.js
+++ musicfinder/src/App.js
@@ -1,44 +1,18 @@
 import React from "react";
 import "./App.css";
-import { Route, Link } from "react-router-dom";
+import { Route } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import Login from "./components/LoginForm/OAuthGoogle.js";
-import YoutubePlayer from "./components/YoutubePlayer/YoutubePlayer.js";
-import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy.js";
-import TermsOfService from "./components/TermsOfService/TermsOfService.js";
-import SignOut from "./components/SignOut/SignOut.js";
-import Browser from "./components/Browser/Browser.js";
-import User from "./components/user/User.js";
-import LandingPage from "./components/LandingPage/LandingPage.js";
 import styled from "styled-components";
 
-import Patreon from "./components/Patreon/Patreon";
+import LandingPage from "./components/LandingPage/LandingPage.js";
+import Navigation from "./components/Navigation/Navigation";
 
 function App() {
   return (
     <Router>
       <RouterDiv className="App">
-        <header className="">
-          <nav className="navBar" style={{ width: "100%", display: "flex" }}>
-            <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
-            <Link to="/browser">Browse Songs</Link>
-            {/* <Link to='/privacypolicy'>Legal Stuff</Link> */}
-            <Link to="/termsofservice">Terms & Services</Link>
-            <Link to="/user">Your Profile</Link>
-            <Link to="/signout">Log Out</Link>
-            <Link to="/support-us">Support Us</Link>
-          </nav>
-          <Route path="/login" component={Login} />
-          <Route path="/player" component={YoutubePlayer} />
-          <Route path="/privacypolicy" component={PrivacyPolicy} />
-          <Route path="/termsofservice" component={TermsOfService} />
-          <Route path="/signout" component={SignOut} />
-          <Route path="/user" component={User} />
-          <Route path="/browser" component={Browser} />
-          <Route path="/support-us" component={Patreon} />
-        </header>
         <Route exact path="/" component={LandingPage} />
+        <Route exact path="/home" component={Navigation} />
       </RouterDiv>
     </Router>
   );
@@ -46,6 +20,7 @@ function App() {
 export default App;
 
 const RouterDiv = styled.div`
+  background: black;
   .navBar {
     display: flex;
     flex-wrap: warp;
@@ -61,7 +36,6 @@ const RouterDiv = styled.div`
     a:hover {
       color: white;
       border-radius: 5px;
-      border: 1px solid grey;
     }
   }
 `;
