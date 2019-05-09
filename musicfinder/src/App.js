9a5aa3d88f767d01b95849c540942f5733fbcd6b
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-05-09 17:10:04 -0600

    forgot to save the changes lol

--- musicfinder/src/App.js
+++ musicfinder/src/App.js
@@ -24,11 +24,6 @@ export default App;
 const RouterDiv = styled.div`
   witdth: 100%;
   min-height: 100vh;
-<<<<<<< HEAD
-  background: #705771;
-`;
-=======
   background: #696773;
   padding-top: 40px;
-`
->>>>>>> 365c3515ef79c8e2c0282a44f7128988030de84d
+`;
