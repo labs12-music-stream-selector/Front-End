434b378adc8206d7b5c2049479f4a5279af8c708
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-05-09 13:27:16 -0600

    just put keys into constant variables MUST REMEMBER NOT TO PULL REQUEST TO MASTER FOR TIME BEING

--- musicfinder/src/components/LoginForm/OAuthGoogle.js
+++ musicfinder/src/components/LoginForm/OAuthGoogle.js
@@ -45,17 +45,17 @@ export default class OAuthGoogle extends Component {
     };
     // Md's Client ID from Google
     // "557783495237-jqq3d269c5ee4uvbg0bv74rs1sb91g90.apps.googleusercontent.com"
+    const CLIENTID_MD =
+      "557783495237-jqq3d269c5ee4uvbg0bv74rs1sb91g90.apps.googleusercontent.com";
 
     // Logan's Client ID from Google
     // "1023911349266-uh3fvbbt7d652443db15q3f477v3oa9v.apps.googleusercontent.com"
+    const CLIENTID_LOGAN =
+      "1023911349266-uh3fvbbt7d652443db15q3f477v3oa9v.apps.googleusercontent.com";
     return (
       <div>
         <GoogleLogin
-          clientId="557783495237-jqq3d269c5ee4uvbg0bv74rs1sb91g90.apps.googleusercontent.com"
-          // discoverDocs={[
-          //   "https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"
-          // ]}
-          // scope="https://www.googleapis.com/auth/youtube.force-ssl"
+          clientId={CLIENTID_LOGAN}
           buttonText="Login with Google"
           onSuccess={responseGoogle}
           onFailure={responseGoogle}
