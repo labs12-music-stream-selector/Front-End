3c5cbbed5cba157020c4e99721184099f035e4ef
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-05-16 17:42:49 -0400

    able to display songs of a playlist

--- musicfinder/src/components/DisplayPlaylist/DisplayPlaylist.js
+++ musicfinder/src/components/DisplayPlaylist/DisplayPlaylist.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
+
+import Track from '../Track/Track.js';
 
 const DisplayPlaylist = (props) => {
+  
+  const [tracks, setTracks] = useState([{
+    url: 'Vrd8uDWRmx0'
+  },
+  {
+    url: 'Vrd8uDWRmx0'
+  },
+  {
+    url: 'Vrd8uDWRmx0'
+  }
+  ]);
+
+  useEffect(() => {
+    // axios.get('url here').then(res => {
+    //   setTracks(res.data)
+    // }).catch(err => console.log(err))
+  },[])
+
   return (
     <DisplayPlaylistContainer>
+      <h2>Playlist</h2>
       <ul>
-        {props.tracks.map(track => {
+        {tracks.map(track => {
           return (
-            <li>
-              <Thumbnail key={props.track.url + props.index} src={thumbnailURL} />
-              <h3>{props.track.track_title}</h3>
-              <p>Mood: {props.track.mood}</p>
+            <li key={track.url}>
+              <Track 
+                inPlaylist
+                track={track}
+                allTracks={props.allTracks}
+                updateCurrentVideo={props.updateCurrentVideo}
+                updateAutoPlay={props.updateAutoPlay}
+              />
             </li>
           )
         })}
@@ -26,5 +52,26 @@ const Thumbnail = styled.img`
 `;
 
 const DisplayPlaylistContainer = styled.div`
-
+  margin-left: 5px;
+  max-width: 95vw;
+  margin: 0 auto;
+  h2{
+    margin: 0;
+    color: #eff1f3;
+  }
+  ul{
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    height: 95%;
+    overflow-y: scroll;
+    @media(max-width: 700px){
+      height: 300px;
+      display: flex;
+      overflow-x: scroll;
+      overflow-y: unset;
+      height: max-content;
+      width: calc(100% -5px);
+    }
+  }
 `;
