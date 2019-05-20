3b88bd48a15d09aa07a3da1272e0d39b69760ee5
lilvina <davinaleo2007@yahoo.com>
2019-05-20 12:23:05 -0700

    added cursor to select mood for songs

--- musicfinder/src/components/SelectMoodDropdown/SelectMoodDropdown.js
+++ musicfinder/src/components/SelectMoodDropdown/SelectMoodDropdown.js
@@ -79,6 +79,7 @@ export default SelectMoodDropdown;
 
 const SelectMoodDropdownDiv = styled.div`
   background-color: rgba(0, 0, 0, 0);
+  cursor: pointer;
   display: inline-block;
   box-sizing: border-box;
   position: relative;
@@ -114,6 +115,7 @@ const SelectMoodListItem = styled.div`
   z-index: 95;
   box-sizing: border-box;
   width: 100%;
+  cursor: pointer;
   background-color: #272727;
   color: ivory;
   list-style: none;
