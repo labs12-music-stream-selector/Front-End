60fa8087e55f97bf1d2bc0f936870378b74a27f6
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-05-20 16:17:08 -0400

    styled add/delete buttons

--- musicfinder/src/components/DisplayPlaylist/DisplayPlaylist.js
+++ musicfinder/src/components/DisplayPlaylist/DisplayPlaylist.js
@@ -15,7 +15,13 @@ const DisplayPlaylist = (props) => {
   return (
     <DisplayPlaylistContainer>
       <h2>Playlist</h2>
-      <button onClick={()=>{addCurrentTrack(props.playlistId, props.currentTrack.video_id)}}>add currentTrack</button>
+      <AddBtn title='add current track to playlist' onClick={()=>{addCurrentTrack(props.playlistId, props.currentTrack.video_id)}}>
+        <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M12 8V16" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M8 12H16" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </svg>
+      </AddBtn>
       <ul>
         {tracks.map(track => {
           return (
@@ -60,6 +66,20 @@ const Thumbnail = styled.img`
 	width: 100px;
 `;
 
+const AddBtn = styled.button`
+  background: none;
+  border: none;
+  svg{
+    path{
+      stroke: white;
+    }
+    :hover{
+      cursor: pointer;
+    }
+  }
+  outline: none;  
+`;
+
 const DisplayPlaylistContainer = styled.div`
   margin-left: 5px;
   max-width: 95vw;
@@ -69,10 +89,12 @@ const DisplayPlaylistContainer = styled.div`
     color: #eff1f3;
   }
   ul{
+    width: min-content;
+    min-width: 310px;
     list-style: none;
     padding: 0;
     margin: 0;
-    height: 95%;
+    height: 86%;
     overflow-y: scroll;
     @media(max-width: 700px){
       height: 300px;
--- musicfinder/src/components/Track/Track.js
+++ musicfinder/src/components/Track/Track.js
@@ -37,7 +37,13 @@ const Track = props => {
             : props.track.video_title}
         </h3>
         {props.inPlaylist ? null : <p>Mood: {props.track.moods}</p>}
-        {props.inPlaylist ? <button onClick = {() => deleteTrack(props.inPlaylist, props.track.id)}>delete</button>: null}
+        {props.inPlaylist ? <DeleteBtn title='remove from playlist' onClick = {() => deleteTrack(props.inPlaylist, props.track.id)}>
+          <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M9 9L15 15" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          <path d="M15 9L9 15" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </DeleteBtn>: null}
       </div>
       {/* <MoodSuggestForm
           video_id={props.track.url}
@@ -97,6 +103,20 @@ const Thumbnail = styled.img`
   :hover {
     cursor: pointer;
   }
+`;
+
+const DeleteBtn = styled.button`
+  background: none;
+  border: none;
+  svg{
+    path{
+      stroke: red;
+    }
+    :hover{
+      cursor: pointer;
+    }
+  }
+  outline: none;
 `;
 
 const TrackContainer = styled.div`
