cc3b5eed1562cb25a8eb5602cd25dc64b80d5855
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-05-21 16:31:59 -0700

    added playlistId to useEffect to make it reload when a new playlist is clicked

--- musicfinder/src/components/DisplayPlaylist/DisplayPlaylist.js
+++ musicfinder/src/components/DisplayPlaylist/DisplayPlaylist.js
@@ -1,25 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
 
-import Track from '../Track/Track.js';
+import Track from "../Track/Track.js";
 
-const DisplayPlaylist = (props) => {
-
+const DisplayPlaylist = props => {
   const [tracks, setTracks] = useState([]);
 
   useEffect(() => {
     fetchTracks();
-  }, [])
+  }, [props.playlistId]);
 
   return (
     <DisplayPlaylistContainer>
       <h2>Playlist</h2>
-      <AddBtn title='add current track to playlist' onClick={()=>{addCurrentTrack(props.playlistId, props.currentTrack.video_id)}}>
-        <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M12 8V16" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M8 12H16" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      <AddBtn
+        title="add current track to playlist"
+        onClick={() => {
+          addCurrentTrack(props.playlistId, props.currentTrack.video_id);
+        }}
+      >
+        <svg
+          width="30"
+          height="30"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M12 8V16"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M8 12H16"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
         </svg>
       </AddBtn>
       <ul>
@@ -36,62 +64,75 @@ const DisplayPlaylist = (props) => {
                 fetchTracks={fetchTracks}
               />
             </li>
-          )
+          );
         })}
       </ul>
     </DisplayPlaylistContainer>
-  )
+  );
 
-    function addCurrentTrack(playlistId, song_id){
-        axios.post(`https://fantabulous-music-finder.herokuapp.com/api/user/playlists/${playlistId}/song`,{song_id}).then(res => {
-            console.log('added successfully')
-            fetchTracks();
-        }).catch(err => console.log(err))
-    }
-
-    function fetchTracks(){
-      axios.get(`https://fantabulous-music-finder.herokuapp.com/api/user/playlists/${props.playlistId}/songs`).then(res => {
-        const newTracksArray = res.data.map(track => {
-          let newTrack = {...track, video_id: track.song_id};
-          return newTrack
-        })
-        setTracks(newTracksArray)
-      }).catch(err => {
-        console.log(err)
-        setTracks([])
+  function addCurrentTrack(playlistId, song_id) {
+    axios
+      .post(
+        `https://fantabulous-music-finder.herokuapp.com/api/user/playlists/${playlistId}/song`,
+        { song_id }
+      )
+      .then(res => {
+        console.log("added successfully");
+        fetchTracks();
       })
-    }
-}
+      .catch(err => console.log(err));
+  }
+
+  function fetchTracks() {
+    axios
+      .get(
+        `https://fantabulous-music-finder.herokuapp.com/api/user/playlists/${
+          props.playlistId
+        }/songs`
+      )
+      .then(res => {
+        const newTracksArray = res.data.map(track => {
+          let newTrack = { ...track, video_id: track.song_id };
+          return newTrack;
+        });
+        setTracks(newTracksArray);
+      })
+      .catch(err => {
+        console.log(err);
+        setTracks([]);
+      });
+  }
+};
 
 export default DisplayPlaylist;
 
 const Thumbnail = styled.img`
-	width: 100px;
+  width: 100px;
 `;
 
 const AddBtn = styled.button`
   background: none;
   border: none;
-  svg{
-    path{
+  svg {
+    path {
       stroke: white;
     }
-    :hover{
+    :hover {
       cursor: pointer;
     }
   }
-  outline: none;  
+  outline: none;
 `;
 
 const DisplayPlaylistContainer = styled.div`
   margin-left: 5px;
   max-width: 95vw;
   margin: 0 auto;
-  h2{
+  h2 {
     margin: 0;
     color: #eff1f3;
   }
-  ul{
+  ul {
     width: min-content;
     min-width: 310px;
     list-style: none;
@@ -99,7 +140,7 @@ const DisplayPlaylistContainer = styled.div`
     margin: 0;
     height: 86%;
     overflow-y: scroll;
-    @media(max-width: 700px){
+    @media (max-width: 700px) {
       height: 300px;
       display: flex;
       overflow-x: scroll;
