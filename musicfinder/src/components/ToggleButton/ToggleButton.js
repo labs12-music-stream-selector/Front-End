7fbc488e3d73f1fcd90d10740e5ec0f775797f9f
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-05-22 15:38:04 -0700

    cleaned out console.log

--- musicfinder/src/components/ToggleButton/ToggleButton.js
+++ musicfinder/src/components/ToggleButton/ToggleButton.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 
 const ToggleButton = props => {
-  console.log(props);
   return (
     <ToggleContainer>
       <ShowSongsButton
