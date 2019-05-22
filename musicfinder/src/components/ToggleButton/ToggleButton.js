dab374ac431be4e56915e71acf7596f97bc52e24
lilvina <davinaleo2007@yahoo.com>
2019-05-22 13:38:38 -0700

    created add playlist button

--- musicfinder/src/components/ToggleButton/ToggleButton.js
+++ musicfinder/src/components/ToggleButton/ToggleButton.js
@@ -7,8 +7,11 @@ const ToggleButton = props => {
       <ToggleContainer>
         <ShowSongsButton onClick={() => {props.updateShowPlaylists(false)}}>Show Songs</ShowSongsButton>
         <ShowPlaylistsButton onClick={() => {props.updateShowPlaylists(true)}}>Show Playlists</ShowPlaylistsButton>
+        <AddPlaylistContainer>
+          <AddPlaylistButton>Add Playlist</AddPlaylistButton>
+        </AddPlaylistContainer>
       </ToggleContainer>
-    
+
   );
 };
 
@@ -29,7 +32,7 @@ const ShowSongsButton = styled.button`
   box-shadow: 0px -8px 10px black;
   color: ivory;
   `;
-  
+
   const ShowPlaylistsButton = styled.button`
   padding: 20px;
   border-radius: 40px 40px 0px 0px;
@@ -39,6 +42,15 @@ const ShowSongsButton = styled.button`
   background-color: #373737;
   color: #dcdcdc;
   box-shadow: inset 0px -2px 4px rgba(0,0,0,1), inset 0px -4px 4px rgba(0,0,0,.5), 0px -2px 4px black;
+`;
+
+const AddPlaylistContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const AddPlaylistButton = styled.button`
+  display: flex;
 `;
 
 export default ToggleButton;
