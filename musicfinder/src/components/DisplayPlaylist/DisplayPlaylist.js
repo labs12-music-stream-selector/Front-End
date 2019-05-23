c54de7bc1db1cae10fe1e010151bf6625d784be2
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-05-22 23:07:14 -0400

    Clean up svgs

--- musicfinder/src/components/DisplayPlaylist/DisplayPlaylist.js
+++ musicfinder/src/components/DisplayPlaylist/DisplayPlaylist.js
@@ -25,34 +25,10 @@ const DisplayPlaylist = props => {
           addCurrentTrack(props.playlistId, props.currentTrack.video_id);
         }}
       >
-        <svg
-          width="30"
-          height="30"
-          viewBox="0 0 24 24"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z"
-            stroke="white"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M12 8V16"
-            stroke="white"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
-          <path
-            d="M8 12H16"
-            stroke="white"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          />
+        <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M19 3H5C3.89543 3 3 3.89543 3 5V19C3 20.1046 3.89543 21 5 21H19C20.1046 21 21 20.1046 21 19V5C21 3.89543 20.1046 3 19 3Z" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M12 8V16" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M8 12H16" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
       </AddBtn>
       <DragDropContext onDragEnd={handleOrderChange}>
@@ -60,7 +36,6 @@ const DisplayPlaylist = props => {
           <Droppable 
             droppableId="droppable" 
             direction={window.innerWidth < 700? 'horizontal' : 'vertical'}
-            
             >
             {(provided, snapshot) => (
               <div 
